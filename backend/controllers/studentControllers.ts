2c3e3e84fe79582f983dffc2856cf2328666c9bf
Paul Abraham <paul.vatakencherry@people10.com>
2022-11-25 12:20:49 +0530

    Refactored lint errors

--- backend/controllers/studentControllers.ts
+++ backend/controllers/studentControllers.ts
@@ -6,9 +6,9 @@ const student = require('../models/studentmodel');
 
 // Get all students
 // GET /api/students
-const getStudent = asyncHandler(async (req: Request, res: Response) => {
-    const projects = await student.find();
-    res.status(200).json(projects);
+const getStudents = asyncHandler(async (req: Request, res: Response) => {
+    const students = await student.find();
+    res.status(200).json(students);
 });
 
 // Create a new student
@@ -18,12 +18,12 @@ const createStudent = asyncHandler(async (req: Request, res: Response) => {
         res.status(400);
         throw new Error('Title is required');
     }
-    const project = await student.create(req.body);
-    if (!project) {
+    const students = await student.create(req.body);
+    if (!students) {
         res.status(400);
         throw new Error('student not created');
     }
-    res.status(201).json(project);
+    res.status(201).json(students);
 });
 
 // Get a student by id
@@ -33,12 +33,12 @@ const getStudent = asyncHandler(async (req: Request, res: Response) => {
         res.status(400);
         throw new Error(`${req.params.id} is not a valid id`);
     }
-    const project = await student.findById(req.params.id);
-    if (!project) {
+    const students = await student.findById(req.params.id);
+    if (!students) {
         res.status(404);
         throw new Error('student not found');
     }
-    res.status(200).json(project);
+    res.status(200).json(students);
 });
 
 //  Delete a student by id
@@ -55,7 +55,6 @@ const deleteStudent = asyncHandler(async (req: Request, res: Response) => {
     }
     res.status(200).json({
         message: `Project ${req.params.id} deleted`,
-        project: project,
     });
 });
 
@@ -70,10 +69,10 @@ const updateStudent = asyncHandler(async (req: Request, res: Response) => {
         res.status(400);
         throw new Error(`${req.params.id} is not a valid id`);
     }
-    const student = await student.findByIdAndUpdate(req.params.id, req.body, {
+    const students = await student.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
     });
-    if (!student) {
+    if (!students) {
         res.status(404);
         throw new Error('student not found');
     }
